380ef5e46822ee77ddeafbd56e5ae8f8b1f9a22c
yunhui0502 <2661234231@qq.com>
2020-12-21 16:44:09 +0800

    重启项目

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,7 +23,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
-    publicPath:  process.env.NODE_ENV === 'production' ? '/Master' : '/',
+    publicPath:  '/Master',
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
